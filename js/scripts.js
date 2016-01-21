de154ecfa58baa29c0e731a54ee5251d14466723
Brad London and Michael Lor <michael.vang.1985@gmail.com>
2016-01-21 09:39:30 -0800

    defined specs for input functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,38 @@
-$(document).ready(function(){
-  var counter = function(number) {
-    number = (number * 1)
-    return number;
+var counter = function(number) {
+  number = (number * 2)
+  return number;
+}
+
+var range = function(countTo, countBy) {
+  var countTo;
+  var countBy;
+  var result = parseInt(countTo / countBy)
+  return result;
+}
+
+var result = function(a,b) {
+
+  var numberArray = [];
+  var countArray = [];
+  var range = function(countTo, countBy) {
+    var countTo;
+    var countBy;
+    var result = parseInt(countTo / countBy)
+    return result;
   }
+
+
+  for(var i = 1; i < range; i++) {
+    if ()
+
+    var output = ();
+    return output;
+  }
+
+
+
+}
+
+$(document).ready(function(){
+
 })
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,11 @@
 describe('counter', function() {
   it("will multiply by 1.", function(){
-  expect(counter(5)).to.equal(5);
+  expect(counter(5)).to.equal(10);
+});
+});
+
+describe('range', function() {
+  it("will divide the count-to number by the count-by input", function(){
+  expect(range(50,7)).to.equal(7);
 });
 });
